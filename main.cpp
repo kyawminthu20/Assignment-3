96b4e233c9382e959fdd8da57b137d93211a720d
Kyaw Min Thu <kyawminthu.kn@gmail.com>
2021-11-10 18:01:44 +0000

    Data reading to 2D array

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <filesystem>
+#include <stdlib.h>
 
 using namespace std;
+using std::filesystem::exists;
 
 
 void read_line(int &line_count, string filename, vector<string> &line_data)
@@ -26,6 +29,55 @@ void read_line(int &line_count, string filename, vector<string> &line_data)
   return;
 }
 
+void read_file()
+{
+  bool valid_file = false;
+  string file_name;
+  int count_line = 0;
+  
+
+  do
+  {
+
+    cout << "Enter a file name : ";
+    cin >> file_name;
+    if(file_name == "exit")
+      exit(EXIT_FAILURE);
+
+    //Check File and validate file
+    ifstream dataIn;
+    dataIn.open(file_name);
+    if(dataIn.good())
+    {
+      valid_file = true;
+      cout << file_name << " -- File Exist" << endl;
+
+    }
+    else
+    {
+      cout << file_name <<  " -- File doesn't exist" << endl;
+    }
+    dataIn.close();
+
+  }while(valid_file != true);
+
+  // Check how many lines
+
+
+
+  // Importing data
+  
+
+ 
+
+
+
+
+}
+
+
+
+
 string vector_to_string(vector<char> temp)
 {
   string s;
@@ -47,6 +99,8 @@ void print_quotes(vector<string> line_data, vector<vector<char>> data, int line_
     cout << endl;
   }
 }
+
+
 
 void search_quotes(vector<string> line_data, vector<vector<char>> data, int line_count, string s)
 {
@@ -74,38 +128,45 @@ int main()
 
 {
 
-  string file_name = "quotes.txt";
-  int line_count = 0;
-  vector<vector<char>> data;
+  read_file();
 
-  // vector array to accept the data from reading the file
-  vector<string> line_data; 
-  //Read the data and store it in the vector array with reference
-  read_line(line_count, file_name,line_data); 
-  
-  //cout << line_count << endl;
 
-  for(int i = 0; i < line_count ; i++)
-  {
-    vector<char> temp;
+  /*
 
-    //cout << line_data[i].length() << endl;
-    //cout << line_data[i] << endl;
-    for(int j = line_data[i].length(); j >= 0; j--)
+    string file_name = "quotes.txt";
+    int line_count = 0;
+    vector<vector<char>> data;
+
+    // vector array to accept the data from reading the file
+    vector<string> line_data; 
+    //Read the data and store it in the vector array with reference
+    read_line(line_count, file_name,line_data); 
+    
+    //cout << line_count << endl;
+
+    for(int i = 0; i < line_count ; i++)
     {
-      temp.push_back(line_data[i][j]);
-      //cout << line_data[i][j];
+      vector<char> temp;
+
+      //cout << line_data[i].length() << endl;
+      //cout << line_data[i] << endl;
+      for(int j = line_data[i].length(); j >= 0; j--)
+      {
+        temp.push_back(line_data[i][j]);
+        //cout << line_data[i][j];
+      }
+      data.push_back(temp);
+      //cout << endl << endl;
     }
-    data.push_back(temp);
-    //cout << endl << endl;
-  }
 
-  print_quotes(line_data,data,line_count);
+    print_quotes(line_data,data,line_count);
 
-  string s;
-  cout << "Enter Search Word : ";
-  getline(cin,s);
-  search_quotes(line_data,data,line_count,s);
+    string s;
+    cout << "Enter Search Word : ";
+    getline(cin,s);
+    search_quotes(line_data,data,line_count,s);
+
+  */
  
   return 0;
 }
